7ea1827ef0cd632cba605a18b9568b03e24e5864
LucasBartoszewski <khoga91@gmail.com>
2014-06-10 17:44:45 +0200

    first commit

---
+++ Scripts/_references.js
@@ -0,0 +1,7 @@
+﻿/// <reference path="jquery.fancybox.js" />
+/// <reference path="jquery-2.1.1.js" />
+/// <reference path="jquery-ui-1.10.4.js" />
+/// <autosync enabled="true" />
+/// <reference path="modernizr-2.6.2.js" />
+/// <reference path="bootstrap.js" />
+/// <reference path="respond.js" />
